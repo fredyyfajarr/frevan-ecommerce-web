b6b8f1ce7a317536a08793672d6060f7e25fb89a
fredyyfajarr <fredyfajar46@gmail.com>
2026-06-07 14:55:41 +0700

    Send auth token with API requests

--- src/api.js
+++ src/api.js
@@ -5,6 +5,19 @@ const customAPI = axios.create({
   withCredentials: true,
 });
 
+customAPI.interceptors.request.use((config) => {
+  try {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user?.token && !config.headers.Authorization) {
+      config.headers.Authorization = `Bearer ${user.token}`;
+    }
+  } catch {
+    localStorage.removeItem('user');
+  }
+
+  return config;
+});
+
 // const customAPI = axios.create({
 //   baseURL: 'http://localhost:3000/api/v1', // Explicitly set backend URL
 //   withCredentials: true, // Required for cookies
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -10,7 +10,10 @@ const userSlice = createSlice({
   initialState,
   reducers: {
     loginUser: (state, action) => {
-      const user = action.payload.data;
+      const user = {
+        ...action.payload.data,
+        token: action.payload.token,
+      };
       // set value from state
       state.user = user;
       // set local storage
@@ -23,7 +26,10 @@ const userSlice = createSlice({
       toast.success('Logout Success');
     },
     registerUser: (state, action) => {
-      const user = action.payload.data;
+      const user = {
+        ...action.payload.data,
+        token: action.payload.token,
+      };
       state.user = user;
       // set local storage
       localStorage.setItem('user', JSON.stringify(user));
